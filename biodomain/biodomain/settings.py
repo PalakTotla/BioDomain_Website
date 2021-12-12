85dd190dabbd3bdeb249b7e1fbc94c554c9f7911
Barun-S <66858557+Barun-S@users.noreply.github.com>
2021-12-12 23:20:58 +0530

    c

--- biodomain/biodomain/settings.py
+++ biodomain/biodomain/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'contactbioequipzone@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_HOST_USER = 'contactbioequipzone@gmail.com' 
-EMAIL_HOST_PASSWORD = '<password>' 
+EMAIL_HOST_PASSWORD = '<contactbioequipzone_password>' 
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True 
